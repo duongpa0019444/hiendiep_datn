f1d21ae298aefb1cbc5131582f58a2e9cf0b978b
unknown <lethitrangfpt2005@gmail.com>
2025-06-26 14:46:11 +0700

    trang

--- app/Http/Controllers/client/HomeController.php
+++ app/Http/Controllers/client/HomeController.php
@@ -5,13 +5,18 @@ namespace App\Http\Controllers\client;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\courses;
+
 
 use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
+{public function index()
 {
-    public function index(){
-        // $data = DB::table('san_phams')->select('*')->get();
-        return view('client/home');
-    }
+    $featuredCourses = courses::where('is_featured', 1)->latest()->limit(6)->get(); 
+    
+
+    return view('client.home', compact('featuredCourses'));
 }
+}
+
---
+++ resources/views/admin/course/lession-add.blade.php
@@ -0,0 +1,80 @@
+@extends('admin.admin')
+
+@section('title', 'Trang admin')
+@section('description', '')
+@section('content')
+
+<div class="page-content">
+    <form action="{{route('admin.lession-add', ['id' => $id])}}" method="POST">
+        @csrf
+
+        <!-- Start Container Fluid -->
+        <div class="container-xxl">
+            <div class="row">
+                 @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul class="mb-0">
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                <div class="col-xl-12 col-lg-10">
+                    <!-- Thêm thông tin -->
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Thêm thông tin</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <!-- Tên khóa học -->
+                                <div class="col-lg-6">
+                                    <div class="mb-3">
+                                        <label for="name" class="form-label">Tên bài giảng </label>
+                                        <input type="text" name="name" id="name" class="form-control" placeholder="Nhập tên bài giảng ">
+                                    </div>
+                                </div>
+
+                                <!-- Giá khóa học -->
+                                <div class="col-lg-6">
+                                    <div class="mb-3">
+                                        <label for="link_document" class="form-label">Tài liệu học</label>
+                                        <input type="text" name="link_document" id="link_document" class="form-control" placeholder="TTài liệu học">
+                                    </div>
+                                </div>
+
+                                <!-- Ngày cập nhật -->
+                                <div class="col-lg-6">
+                                    <div class="mb-3">
+                                        <label for="updated_at" class="form-label">Ngày cập nhật</label>
+                                        <input type="datetime-local" name="updated_at" id="updated_at" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Buttons -->
+                    <div class="p-3 bg-light mb-3 rounded">
+                        <div class="row justify-content-end g-2">
+                            
+                            <div class="col-lg-2">
+                                <button type="reset" class="btn btn-primary w-100">Cancel</button>
+                            </div>
+
+                            <div class="col-lg-2">
+                                <button type="submit" class="btn btn-outline-secondary w-100">Save Change</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+@endsection
+
